32423ae153a24825b26cebc88abb0e0cda84d86c
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2024-12-08 22:27:03 +0100

    Config: Update Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(
     {
       webAnalytics: {
